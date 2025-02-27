4c914270c4f637adab7e9c82fed9a07b83a5482c
Siphosethu Dyasi <siphosethudyasi@gmail.com>
2025-02-27 11:19:37 +0200

    init

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { theme } from "@/theme/theme-style";
+import '../global.css';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
